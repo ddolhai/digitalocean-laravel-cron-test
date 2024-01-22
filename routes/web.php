849a5ad06d8115e50250b9fa0c44b180ffe7cf80
Dolhai Dávid <ddolhai.work@gmail.com>
2024-01-22 08:51:45 +0100

    feat: trigger-job added

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/trigger-job', function () {
+    $exitCode = Artisan::call('schedule:run');
+    return response()->json(['message' => 'Scheduler run successfully', 'exitCode' => $exitCode]);
+});
